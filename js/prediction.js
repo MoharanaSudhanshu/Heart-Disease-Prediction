d442f6d59cc550e196b61d4ac3484f680be46500
SUDHANSHU SEKHER MOHARANA <156045025+MoharanaSudhanshu@users.noreply.github.com>
2026-06-06 23:38:31 +0530

    Change prediction API endpoint to remote server
    Updated the prediction API endpoint to a remote server.

--- js/prediction.js
+++ js/prediction.js
@@ -19,7 +19,7 @@ document
       thal: Number(thal.value),
     };
 
-    const response = await fetch("http://127.0.0.1:5000/predict", {
+    const response = await https://heart-disease-prediction-wixh.onrender.com, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
